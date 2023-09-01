0247d2e2ece254ab468efc0a9a7ab74890a930bf
Sana Rzgar <sanarizgar34@gmail.com>
2023-09-01 11:30:43 +0300

    dropdown style change

--- src/Component/dropDown/DropDown.jsx
+++ src/Component/dropDown/DropDown.jsx
@@ -4,45 +4,49 @@ import { addSort} from '../../features/sortSlice';
 import { useDispatch } from 'react-redux';
 import { useGetAllProductsQuery } from '../../features/products';
 function DropDown() {
-//TODO: design must change
-     const dispatch=useDispatch()
-     const [selectedValue, setSelectedValue] = useState('');
-     const options = ['normal', 'desc', 'asc'];
-     const {data:allProducts ,isLoading}=useGetAllProductsQuery()
-  const handleSelectChange = (event) => {
-    if (event.target.value === "normal") {
-        dispatch(addSort(allProducts))
-    } else if (event.target.value === "desc") {
-      const sorttt = [...allProducts].sort((a, b) => b.price - a.price)
-         dispatch(addSort(sorttt))
+//TODO: design must change to be better 
+  const dispatch = useDispatch();
+  const [selectedOption, setSelectedOption] = useState('normal'); 
+  const [showOptions, setShowOptions] = useState(false);
+  const options = ['normal', 'descending', 'ascending'];
+  const { data: allProducts, isLoading } = useGetAllProductsQuery();
+
+  const handleSelectChange = (option) => {
+    setSelectedOption(option);
+
+    if (option === 'normal') {
+      dispatch(addSort(allProducts));
+    } else if (option === 'descending') {
+      const desc = [...allProducts].sort((a, b) => b.price - a.price);
+      dispatch(addSort(desc));
     } else {
-         const asc = [...allProducts].sort((a, b) => a.price - b.price)
-         dispatch(addSort(asc))
+      const asc = [...allProducts].sort((a, b) => a.price - b.price);
+      dispatch(addSort(asc));
     }
-    setSelectedValue(event.target.value)
-        // sort done with api but it was sort id not price ,i changed to sort by price but in front end ;)
-        //  setSelectedValue(event.target.value);
-        //  if (event.target.value === "normal") {
-        //       dispatch(addSort(""))
-        //  } else {
-        //       dispatch(addSort(event.target.value))
-        //  }    
-    };
+
+    setShowOptions(false); 
+  };
+
+
   if (isLoading) return null; 
   return (
-      <div className="dropdown-container">
-      <select
-        className={`dropdown ${selectedValue && 'selected'}`}
-        value={selectedValue}
-        onChange={handleSelectChange}
+       <div className="dropdown-container">
+      <div
+        className={`dropdown ${showOptions ? 'open' : ''}`}
+        onClick={() => setShowOptions(!showOptions)}
       >
-     
-        {options.map((option, index) => (
-          <option key={index} value={option}>
-            {option}
-          </option>
-        ))}
-      </select>
+        {selectedOption}
+        <ul className={`options ${showOptions ? 'show' : ''}`}>
+          {options.map((option, index) => (
+            <li
+              key={index}
+              onClick={() => handleSelectChange(option)}
+            >
+              {option}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
